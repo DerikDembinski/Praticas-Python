9692e217884a1ca5ebca26b3875e1d7698d6c58c
Derik Dembinski <derikdembinski@outlook.com>
2023-12-06 22:05:43 -0300

    Subindo Documentacao

--- Pratica (Site da Web esta acessivel).py
+++ Pratica (Site da Web esta acessivel).py
@@ -1,9 +1,10 @@
+#Importando bibliotecas necessarias 
 import urllib
 import urllib.request
-
 try:
+#tentando acessar o site
     resposta = urllib.request.urlopen('https://pudim.com.br/')
-
+#Retornando as informacoes (conforme o sucesso ou  a falha) 
 except Exception as erro:
     print(f'\033[31mO site do Pudim não esta acessivel no momento\033[m')
 
